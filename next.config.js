79b1d786c6b2d070355f1fafb80917620221cb73
Prashant Singh <prashantsh7014@gmail.com>
2025-07-13 12:12:32 +0530

    update json fie

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: { unoptimized: true },
   reactStrictMode: true,
   // Custom webpack config to suppress specific warnings
   webpack: (config, { isServer }) => {
